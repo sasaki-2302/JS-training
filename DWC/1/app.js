e568779a365ea31a330ee88af4e08462e37a403b
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-06-05 10:16:33 +0000

    curriculum3

--- DWC/1/app.js
+++ DWC/1/app.js
@@ -1,15 +1,42 @@
-let nickname = "taro"
-console.log(nickname)
-nickname = "jiro"
-console.log(nickname)
+// let nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
 
 
-var str = "webcamp" //グローバルスコープ
+// var str = "webcamp" //グローバルスコープ
 
-function foo() {
+// function foo() {
+//   console.log(str)
+//   var y = "hello" //関数スコープ
+// }
+
+// foo()
+// console.log(y)
+
+// function foo() {
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+// for (let i = 0; i < 10; i++) {     
+//   console.log(i) 
+// }
+
+// console.log(i)
+
+var str = "webcamp"
+
+function foo() { 
   console.log(str)
-  var y = "hello" //関数スコープ
+  var str = "dmm webcamp"
+  console.log(str)
 }
 
 foo()
-console.log(y)
