8dfeb76487c5075f263332efabda15cbc3bc06f9
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-06-15 07:41:02 +0000

    curriculum5 task

--- DWC/1/app.js
+++ DWC/1/app.js
@@ -5,7 +5,17 @@
 //   });
 // });
 
-$(function(){
-  $('.box1').show();
-  $('.box1').css({'background-color': '#0000FF'});
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
